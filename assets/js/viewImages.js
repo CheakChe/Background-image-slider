765ee913202abf03a6ab2773d5e1d900355228ff
CheakChe <saumonminiball@gmail.com>
2019-04-19 14:49:09 +0300

    v.2

--- assets/js/viewImages.js
+++ assets/js/viewImages.js
@@ -1,8 +1,9 @@
 var images = new Array();
 var i = 1;
 function viewImages() {
-	images[i] = 'background-image: url(assets/img/bg-img-0'+i+'.png)';
-	document.getElementById("bg-img").style = images[i];
+	images[i] = 'url(assets/img/bg-img-0'+i+'.png) ';
+	document.getElementById("bg-img").style.background= images[i];
+	document.getElementById("bg-img").style.backgroundRepeat= 'no-repeat';
 	i++;
 	if (i >= 10) {i = 1;}
 	setTimeout("viewImages()",200);
